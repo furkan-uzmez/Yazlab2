1add5829c22d60cc53fa7e1fe2840b1169c454a3
furkan-uzmez <furkan.n41@hotmail.com>
2025-11-10 00:21:23 +0300

    add register_router to main_api

--- backend/api/main_api.py
+++ backend/api/main_api.py
@@ -2,10 +2,14 @@ from fastapi import FastAPI
 import uvicorn
 
 from backend.api.login_api import router as login_router
+from backend.api.register_api import router as register_router
+
 
 api = FastAPI()
 
 api.include_router(login_router)
+api.include_router(register_router)
+
 
 if __name__ == "__main__":
     uvicorn.run("backend.main:api", host="127.0.0.1", port=8000, reload=True)
