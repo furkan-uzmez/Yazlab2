7ecd57c5493cc7d91c5a7ebe3d55c42c2db89898
furkan-uzmez <furkan.n41@hotmail.com>
2025-11-11 19:01:37 +0300

    add cors setting to main_api

--- backend/api/main_api.py
+++ backend/api/main_api.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 import uvicorn
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from backend.api.auth.login_api import router as login_router
 from backend.api.auth.register_api import router as register_router
 from database.create_tables import create_tables
 
 api = FastAPI()
+
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # React uygulamanın portunu yaz
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 api.include_router(login_router)
 api.include_router(register_router)
