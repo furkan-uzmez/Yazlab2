6b3496e1fb3bf3cd4002c88d537126b04bd13958
furkan-uzmez <furkan.n41@hotmail.com>
2025-11-12 19:43:35 +0300

    delete  open_db_connection in get_user func

--- backend/func/user/get_user.py
+++ backend/func/user/get_user.py
@@ -1,14 +1,8 @@
-from backend.func.db.connection.open_db_connection import open_db_connection
-
-def get_user(user_name: str):
-    connection = open_db_connection()
-    
+def get_user(connection,user_name: str):    
     cursor = connection.cursor(dictionary=True)
     cursor.execute("SELECT * FROM users WHERE username = %s", (user_name,))
     user_data = cursor.fetchone()
-    
-    connection.close()
-    
+        
     if user_data is not None:
         return user_data
     else:
