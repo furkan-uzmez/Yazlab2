c83d85155d163bb805cb0344aa4afc3125a1d78f
furkan-uzmez <furkan.n41@hotmail.com>
2025-11-12 11:02:30 +0300

    add follow func

---
+++ backend/func/user/follow.py
@@ -0,0 +1,23 @@
+def follow(connection, follower_id, followee_id) -> bool:
+    """
+    Make follower_id follow followee_id.
+
+    :param connection: Database connection object
+    :param follower_id: ID of the user who wants to follow
+    :param followee_id: ID of the user to be followed
+    :return: True on success, False on failure
+    """
+    
+    try:
+        with connection.cursor() as cursor:
+            sql = "INSERT INTO follows (follower_id, followed_id) VALUES (%s, %s)"
+            cursor.execute(sql, (follower_id, followee_id))
+        
+        connection.commit()
+        return True # İşlem başarılı
+        
+    except Exception as e:
+        # Hata oluştu (örn: PRIMARY KEY ihlali - zaten takip ediyor)
+        print(f"HATA: Takip etme işlemi başarısız: {e}")
+        connection.rollback() # Değişiklikleri geri al
+        return False # İşlem başarısız
