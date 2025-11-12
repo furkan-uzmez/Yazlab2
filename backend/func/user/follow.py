77eee1e22594f106f6c1ab3b033957e25f857b40
furkan-uzmez <furkan.n41@hotmail.com>
2025-11-12 11:14:17 +0300

    add unfollow_func

--- backend/func/user/follow.py
+++ backend/func/user/follow.py
@@ -4,7 +4,7 @@ def follow(connection, follower_id, followee_id) -> bool:
 
     :param connection: Database connection object
     :param follower_id: ID of the user who wants to follow
-    :param followee_id: ID of the user to be followed
+    :param followed_id: ID of the user to be followed
     :return: True on success, False on failure
     """
     
@@ -19,5 +19,30 @@ def follow(connection, follower_id, followee_id) -> bool:
     except Exception as e:
         # Hata oluştu (örn: PRIMARY KEY ihlali - zaten takip ediyor)
         print(f"HATA: Takip etme işlemi başarısız: {e}")
+        connection.rollback() # Değişiklikleri geri al
+        return False # İşlem başarısız
+
+
+def unfollow(connection, follower_id, followed_id) -> bool:
+    """
+    Make follower_id unfollow followee_id.
+
+    :param connection: Database connection object
+    :param follower_id: ID of the user who wants to unfollow
+    :param followed_id: ID of the user to be unfollowed
+    :return: True on success, False on failure
+    """
+    
+    try:
+        with connection.cursor() as cursor:
+            sql = "DELETE FROM follows WHERE follower_id = %s AND followed_id = %s"
+            cursor.execute(sql, (follower_id, followed_id))
+        
+        connection.commit()
+        return True # İşlem başarılı
+        
+    except Exception as e:
+        # Hata oluştu
+        print(f"HATA: Takipten çıkma işlemi başarısız: {e}")
         connection.rollback() # Değişiklikleri geri al
         return False # İşlem başarısız
