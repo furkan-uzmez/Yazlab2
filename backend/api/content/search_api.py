fa5189ddaecc15d1af4992614ec34981268f6156
furkan-uzmez <furkan.n41@hotmail.com>
2025-11-21 01:15:43 +0300

    add search_book and search_movie to search_api and add search_api router to main_api

--- backend/api/content/search_api.py
+++ backend/api/content/search_api.py
@@ -1,21 +1,32 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status , Query
 from pydantic import BaseModel
 
-from backend.func.content.search import search_content as search_content_func
+from backend.func.content.search_movie import search_movie as search_movie_func
+from backend.func.content.search_book import search_book as search_book_func
 
 router = APIRouter(prefix="/content", tags=["content"])
 
 
 @router.get("/search")
-async def search(query: str):
+async def search(
+    query: str = Query(...),
+    api_type: str = Query(...)
+):
+    print(f"INFO: İçerik araması yapılıyor: {query} için {api_type}")
+
     try:
-        results = search_content_func(query)
+        if api_type == "movie":
+            results = search_movie_func(query)
+        elif api_type == "book":
+            results = search_book_func(query)
+        else:
+            raise HTTPException(
+                status_code=400,
+                detail="Invalid api_type. Supported: movie, book."
+            )
     except Exception as e:
-        print(f"HATA: İçerik araması yapılamadı: {e}")
-        raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Could not perform content search"
-        )   
+        print(f"HATA: {e}")
+        raise HTTPException(500, "Could not perform content search")
 
     if results is None:
         raise HTTPException(
